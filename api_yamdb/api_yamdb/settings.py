1e8b5c22bdce7019e08911a115bfc847bc8f11c7
deorz <TheDamnation1044@yandex.ru>
2022-07-12 11:38:08 +0300

    Removed test dotenv

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,11 +1,9 @@
 import os
 from datetime import timedelta
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-load_dotenv(os.path.join(BASE_DIR, '../infra/.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
